8168f53cf88b4bf92d431c0721235560e31bde90
Code_with_keshav <keshavdhamecha45@gmail.com>
2025-03-21 10:36:05 +0530

    Update LinkList.java
    updated LinkedList

--- LinkList.java
+++ LinkList.java
@@ -1,23 +1,24 @@
 package com.collectionfarmwork;
 
 public class LinkList {
-   // Insert Node in between linked list
+	// Insert Node in between linked list
 	Node head;
 	private int size;
-	
-	class Node{
+
+	class Node {
 		String data;
 		Node next;
-		
-		 Node(String data) {
+
+		Node(String data) {
 			this.data = data;
 			this.next = null;
 			size++;
 		}
 	}
+//=============================================================================
 	public void addFirst(String data) {
 		Node newNode = new Node(data);
-		if(head==null) {
+		if (head == null) {
 			newNode.next = head;
 			head = newNode;
 			return;
@@ -25,180 +26,237 @@ public class LinkList {
 		newNode.next = head;
 		head = newNode;
 	}
+//================================================================================
 //	add element in last of linked list
 	public void addLast(String data) {
 		Node lastNode = new Node(data);
 		// corner case
-		if(head==null) {
+		if (head == null) {
 			lastNode.next = head;
 			head = lastNode;
 			return;
 		}
 		Node secondLast = head;
-		//secondLast.next = head.next;
-		while(secondLast.next!=null) {
-			secondLast  = secondLast.next;
-		
+		// secondLast.next = head.next;
+		while (secondLast.next != null) {
+			secondLast = secondLast.next;
+
 		}
 		secondLast.next = lastNode;
 	}
+//======================================================================================
 	// delete last element from linked list
 	public void removeLast() {
-		if(head==null) {
+		if (head == null) {
 			System.out.println("The List IS Empty");
 			return;
 		}
 		size--;
-		if(head.next == null) {
+		if (head.next == null) {
 			System.out.println("Only one node present in list");
 			head = null;
 			return;
 		}
 		Node secondLast = head;
 		Node last = secondLast.next;
-	
-		while(last.next != null) {
+
+		while (last.next != null) {
 			secondLast = secondLast.next;
 			last = last.next;
 		}
-	    secondLast.next = null;
+		secondLast.next = null;
 	}
+//============================================================================================
 	// remove first element from the list
 	public void removeFirst() {
-		if(head==null) {
+		if (head == null) {
 			System.out.println("The List IS Empty ");
 			return;
 		}
 		size--;
-		if(head.next == null) {
+		if (head.next == null) {
 			System.out.println("Only one node present in list");
 			head = null;
 			return;
 		}
-		
+
 		head = head.next;
-		
+
 	}
-	public void addInBet(int index,String data) {
-		if( index<0 || index>size )
-		{
+//=================================================================================================
+	public void addInBet(int index, String data) {
+		if (index < 0 || index > size) {
 			System.out.println("Index Out Of Bond !");
 			return;
 		}
-		
+
 		Node newNode = new Node(data);
-		size++;
+		//size++;
 		// corner case 1
-		if(head==null || index==0) {
+		if (head == null || index == 0) {
 			newNode.next = head;
 			head = newNode;
 			return;
 		}
 		// coner case 2
-		if(head.next==null && index==1) {
-			head.next = newNode; 
+		if (head.next == null && index == 1) {
+			head.next = newNode;
 			return;
 		}
-	    Node currNode = head;
-	    Node nextNode = currNode.next;
-		for(int i=1;i<size;i++) {
-			 
-			if(i==index) {
-				
+		Node currNode = head;
+		Node nextNode = currNode.next;
+		for (int i = 1; i < size; i++) {
+
+			if (i == index) {
+
 				currNode.next = newNode;
 				newNode.next = nextNode;
 				break;
 			}
 			currNode = currNode.next;
-		    nextNode = nextNode.next;
+			nextNode = nextNode.next;
 		}
 	}
-	public void reverseList(Node head) {
-		if(head==null)return;
-		reverseList(head.next);
-		System.out.println(head.data+" ");
+//=================================================================================================
+	public LinkList reverseList(Node head) {
+//		if (head == null)return;
+//		reverseList(head.next);
+//		System.out.println(head.data+" ");    
+		
+
+		LinkList newList = new LinkList();
+		
+		for (int j = size; j > 0; j--) {
+			Node newNode = head;
+			for (int i = 0; i < j-1; i++) {
+				newNode = newNode.next;
+			}
+			newList.addLast(newNode.data);
+		}
+		return newList;
+	}
+//===========================================================================================
+	public void reverseItretive() {
+		if(head==null || head.next==null)return;
+		
+		Node prevNode = head;
+		Node currNode = head.next;
+		while(currNode != null) {
+			Node nextNode = currNode.next;
+			     currNode.next = prevNode;
+			     
+			     //update nodes
+			     prevNode = currNode;
+			     currNode = nextNode;
+		}
+		head.next = currNode;
+		head = prevNode;
+		
+	}
+//============================================================================
+	public Node reverseRecursive(Node head) {
+		if(head==null || head.next==null )return head;
+		Node newNode = reverseRecursive(head.next);
+		head.next.next = head;
+		head.next = null;
+		
+		return newNode;
 	}
 	
+//=============================================================================================
 	public String getIndex(String data) {
-		if(head==null) {
-			return "The list is empty !" ;
+		if (head == null) {
+			return "The list is empty !";
 		}
 		Node getNode = head;
 		try {
-		for(int i=0;i<size;i++) {
-		
-		if(getNode.data == data) {
-			return data+" found at index :"+i ;
-		  }
-	
-		getNode = getNode.next;
-		}
-		return data+" Not found in this list";
-		}catch(NullPointerException e) {
-			return data+" Not found in this list";
+			for (int i = 0; i < size; i++) {
+
+				if (getNode.data == data) {
+					return data + " found at index :" + i;
+				}
+
+				getNode = getNode.next;
+			}
+			return data + " Not found in this list";
+		} catch (NullPointerException e) {
+			return data + " Not found in this list";
 		}
 	}
-	
+//=============================================================================================
 	public void printList() {
-		if(head==null) {
+		if (head == null) {
 			System.out.println("The List IS Empty Now");
 			return;
 		}
 		Node currNode = head;
-		while(currNode != null ) {
-			System.out.print(currNode.data+"-->");
+		while (currNode != null) {
+			System.out.print(currNode.data + "-->");
 			currNode = currNode.next;
 		}
 		System.out.println("null");
 	}
-	  
+//========================================================================================================
 	public int getSize() {
 		return size;
 	}
-	
-	
-	
-	
+//==========================================================================================
 	public static void main(String[] args) {
-		 
+
 		LinkList list = new LinkList();
-		
+
 		// add first
 		list.addFirst("is");
 		list.addFirst("This");
 		list.printList();
-		
+
 		// add last
 		list.addLast("LinkedList");
 		list.printList();
+
+		// remove last
+		// list.removeLast();
+		// list.printList();
+
+		// remove First
+		// list.removeFirst();
+		// list.printList();
+
 		
-		//remove last
-		list.removeLast();
-	    //list.printList();
-	    
-	    //remove First 
-	    list.removeFirst();
-	    //list.printList();
-	    
-	    System.out.println(list.getSize());
-	    list.addInBet(0, "This");
-	    list.addInBet(2, "List");
-	    list.addInBet(2, "a");
-	    list.printList();
-	      
-	    
-	    LinkList intLL = new LinkList();
-	    intLL.addInBet(0, "6");
-	    intLL.addInBet(1, "8");
-	    intLL.addInBet(2, "3");
-	    intLL.addInBet(3, "7");
-	    intLL.addInBet(4, "5");
-	    
-	    System.out.println(intLL.getIndex("1"));
-	    
-	   intLL.reverseList(intLL.head); 
-	   System.out.println("some changes in codes !!");
-	   
-		 
+
+		
+		list.addInBet(0, "This");
+		list.addInBet(2, "List");
+		list.addInBet(2, "a");
+		list.printList();
+		System.out.println(list.getSize());
+		
+		LinkList intLL = new LinkList();  
+		intLL.addInBet(0, "6");
+		intLL.addInBet(1, "8");
+		intLL.addInBet(2, "3");
+		intLL.addInBet(3, "7");
+		intLL.addInBet(4, "5");
+		System.out.println(intLL.getSize());
+		System.out.println(intLL.getIndex("1"));
+        
+		//LinkList reversedList = intLL.reverseList(intLL.head);
+		intLL.printList();
+		//reversedList.printList();
+		intLL.reverseList(intLL.head).printList();  
+		
+		intLL.reverseItretive();
+		intLL.printList();
+		intLL.reverseItretive();
+		intLL.printList();		
+		
+        intLL.head = intLL.reverseRecursive(intLL.head);
+        intLL.printList();
+
+	}
+	
+	public void check(Node head) {
+		Node newNode = head;
+		System.out.println(newNode.next.data);
 	}
 }
